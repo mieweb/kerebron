41ef04b26b7458dbb989c03c758a5aae8d4e0fdb
Grzegorz Godlewski <gg@gitgis.com>
2025-07-09 21:54:38 +0200

    Remove automerge dep from image node

--- packages/extension-basic-editor/src/NodeImage.ts
+++ packages/extension-basic-editor/src/NodeImage.ts
@@ -1,29 +1,9 @@
 import { type Node as PmNode, type NodeSpec } from 'prosemirror-model';
 import { Node } from '@kerebron/editor';
-import { next as automerge } from '@automerge/automerge/slim';
-
-import { BlockMarker } from '@kerebron/extension-automerge';
 
 export class NodeImage extends Node {
   override name = 'image';
   requires = ['doc'];
-
-  automerge = {
-    block: 'image',
-    isEmbed: true,
-    attrParsers: {
-      fromAutomerge: (block: BlockMarker) => ({
-        src: block.attrs.src?.toString() || null,
-        alt: block.attrs.alt,
-        title: block.attrs.title,
-      }),
-      fromProsemirror: (node: PmNode) => ({
-        src: new automerge.RawString(node.attrs.src),
-        alt: node.attrs.alt,
-        title: node.attrs.title,
-      }),
-    },
-  };
 
   override getNodeSpec(): NodeSpec {
     return {
