b50b3f9f5feda7cae65fb57ab272b0b3020fd841
Grzegorz Godlewski <gg@gitgis.com>
2025-08-31 20:09:22 +0200

    Add test utils package

---
+++ packages/test-utils/src/test-utils.ts
@@ -0,0 +1,12 @@
+export { assertEquals } from '@std/assert';
+
+export function trimLines(args: TemplateStringsArray) {
+  const lines = args[0].split('\n');
+  if (!lines[0].trim()) {
+    lines.shift();
+  }
+  if (!lines[lines.length - 1].trim()) {
+    lines[lines.length - 1] = '';
+  }
+  return lines.join('\n');
+}
