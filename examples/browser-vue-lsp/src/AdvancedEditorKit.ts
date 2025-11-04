3894640b9805ec34157aa71cf26e8e04cfa634a8
Grzegorz Godlewski <gg@gitgis.com>
2025-11-04 18:53:54 +0100

    Update lsp example

---
+++ examples/browser-vue-lsp/src/AdvancedEditorKit.ts
@@ -0,0 +1,67 @@
+import { dracula } from 'thememirror';
+
+import { Extension } from '@kerebron/editor';
+import { ExtensionBasicEditor } from '@kerebron/extension-basic-editor';
+import { ExtensionMarkdown } from '@kerebron/extension-markdown';
+import { ExtensionOdt } from '@kerebron/extension-odt';
+import { ExtensionTables } from '@kerebron/extension-tables';
+import { ExtensionDevToolkit } from '@kerebron/extension-dev-toolkit';
+import { ExtensionMenuLegacy } from '@kerebron/extension-menu-legacy';
+import { Dropdown, MenuElement, MenuItem } from '@kerebron/extension-menu';
+import {
+  ExtensionCodeMirror,
+  NodeCodeMirror,
+} from '@kerebron/extension-codemirror';
+
+export class AdvancedEditorKit extends Extension {
+  override name = 'advanced-editor';
+
+  requires = [
+    new ExtensionBasicEditor(),
+    new ExtensionMarkdown(),
+    new ExtensionOdt(),
+    new ExtensionTables(),
+    new ExtensionDevToolkit(),
+    new ExtensionMenuLegacy({
+      modifyMenu: (menus: MenuElement[][]) => {
+        const fileMenu = [
+          new MenuItem({
+            label: 'Simulate loadDoc',
+            enable: () => true,
+            run: () => this.loadDoc(),
+          }),
+          new MenuItem({
+            label: 'Load',
+            enable: () => true,
+            run: () => this.loadDoc2(),
+          }),
+        ];
+        menus[0].unshift(new Dropdown(fileMenu, { label: 'File' }));
+        return menus;
+      },
+    }),
+    new ExtensionCodeMirror({
+      theme: [dracula],
+    }),
+  ];
+
+  async loadDoc() {
+    const buffer = new TextEncoder().encode(
+      '# TEST \n\n1.  aaa **bold**\n2.  bbb\n\n```js\nconsole.log("TEST")\n```\n',
+    );
+    await this.editor.loadDocument('text/x-markdown', buffer);
+    return true;
+  }
+
+  loadDoc2() {
+    const input: HTMLInputElement = document.createElement('input');
+    input.type = 'file';
+    input.addEventListener('change', async (e) => {
+      const file = e.target.files[0];
+      console.log('Selected file:', file);
+      await this.editor.loadDocument(file.type, await file.bytes());
+    });
+    input.click();
+    return true;
+  }
+}
---
+++ examples/browser-vue-lsp/src/LspEditorKit.ts
@@ -0,0 +1,95 @@
+import { CoreEditor, Extension } from '@kerebron/editor';
+import type { AnyExtensionOrReq, TextRange } from '@kerebron/editor';
+import {
+  createRegexMatcher,
+  ExtensionAutocomplete,
+} from '@kerebron/extension-autocomplete';
+
+import { simpleLspWebSocketTransport } from '@kerebron/extension-codemirror/lsp';
+import { ExtensionLsp } from '@kerebron/extension-lsp';
+
+import type { Token } from '@kerebron/extension-markdown';
+import { SmartOutput } from '@kerebron/editor/utilities';
+import { Transport } from '../../../packages/extension-lsp/src/lspClient.ts';
+
+export class LspEditorKit extends Extension {
+  override name = 'lsp-kit';
+  requires: AnyExtensionOrReq[];
+
+  md: string = '';
+
+  static async createFrom(config?: ConstructorParameters<typeof Extension>[0]) {
+    const protocol = globalThis.location.protocol === 'http:' ? 'ws:' : 'wss:';
+    const lspTransport = await simpleLspWebSocketTransport(
+      protocol + '//' + globalThis.location.host + '/lsp',
+    );
+
+    return new LspEditorKit(config, lspTransport);
+  }
+
+  constructor(
+    config?: ConstructorParameters<typeof Extension>[0],
+    lspTransport: Transport,
+  ) {
+    super(config);
+
+    const autocomplete = new ExtensionAutocomplete({
+      matchers: [createRegexMatcher([/\w*/, /(^|\s)@\w*/])],
+      getItems(query: string) {
+        console.log('query', query);
+        return [
+          '@alice',
+          '@bob',
+          '@doug',
+          '@greg',
+          '@monika',
+        ].filter((str) => str.startsWith(query));
+      },
+      onSelect: (selected: string, range: TextRange) => {
+        this.editor.run.replaceRangeText(range, selected);
+      },
+    });
+
+    this.requires = [
+      autocomplete,
+      new ExtensionLsp({ lspTransport }),
+    ];
+  }
+
+  override setEditor(editor: CoreEditor) {
+    super.setEditor(editor);
+
+    this.editor.addEventListener('selection', async (ev: CustomEvent) => {
+      const selection = ev.detail.selection;
+      if (selection) {
+        const from = Math.min(selection.$anchor.pos, selection.$head.pos);
+        const to = Math.max(selection.$anchor.pos, selection.$head.pos);
+        // console.log('lsp.sel', from, to);
+      }
+    });
+
+    this.editor.addEventListener(
+      'transaction',
+      async (ev: CustomEvent<any>) => {
+        // this.lastValue = ev.detail.transaction.doc;
+        const buffer = await this.editor.saveDocument('text/x-markdown');
+        this.md = new TextDecoder().decode(buffer);
+        // this.$emit('input', this.lastValue);
+      },
+    );
+
+    this.editor.addEventListener('md:output', async (ev: CustomEvent<any>) => {
+      const output: SmartOutput<Token> = ev.detail.output;
+      const mapping = output
+        .getMetas()
+        .filter((meta) =>
+          +(meta?.item?.map?.length) > 0 && meta?.item?.type === 'text'
+        )
+        .map((meta) => ({
+          pmPos: meta.item?.map?.[0] || -1,
+          mdPos: meta.pos,
+          item: meta.item,
+        }));
+    });
+  }
+}
