2bcdc727432f30bc11cb513da885b134f0348c37
Grzegorz Godlewski <gg@gitgis.com>
2026-03-09 17:39:14 +0100

    Add cjs package

--- packages/legacy-compat/vite.config.ts
+++ packages/legacy-compat/vite.config.ts
@@ -1,8 +1,13 @@
+import path from 'node:path';
+import fs from 'node:fs';
+
 import { defineConfig } from 'vite';
 import wasm from 'vite-plugin-wasm';
 
 import { generateAlias } from '../../build/vite-plugins/generateAlias.ts';
 import { deno } from '../../build/vite-plugins/denoPlugin.ts';
+
+const __dirname = import.meta.dirname!;
 
 const SEPARATE_CSS = true;
 
@@ -22,7 +27,7 @@ export default defineConfig({
     lib: {
       name: 'kerebron',
       entry: __dirname + '/mod.ts',
-      formats: ['es'],
+      formats: ['es', 'cjs'],
       fileName: 'kerebron',
     },
     rollupOptions: {
@@ -31,11 +36,23 @@ export default defineConfig({
         auto: __dirname + '/kerebron-auto.css',
         light: __dirname + '/kerebron-light.css',
       },
-      output: {
-        entryFileNames: (chunk) => {
-          if (chunk.name === 'mod') return 'kerebron.js';
-          return '[name].js';
+      plugins: [
+        {
+          name: 'rename-file-after-build',
+          closeBundle() {
+            const distDir = path.resolve(__dirname, 'dist');
+            fs.renameSync(
+              path.join(distDir, 'kerebron3.js'),
+              path.join(distDir, 'kerebron.js'),
+            );
+            fs.renameSync(
+              path.join(distDir, 'kerebron3.cjs'),
+              path.join(distDir, 'kerebron.cjs'),
+            );
+          },
         },
+      ],
+      output: {
         assetFileNames: (assetInfo) => {
           if (assetInfo.name === 'auto.css') {
             return 'kerebron.css';
