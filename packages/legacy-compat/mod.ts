09126dfaaae715c587cc4ddc06c2fd156cac7683
Grzegorz Godlewski <gg@gitgis.com>
2026-03-13 19:27:46 +0100

    feat: export ExtensionHistory in compat lib (#80)
    Resolves: #80

--- packages/legacy-compat/mod.ts
+++ packages/legacy-compat/mod.ts
@@ -1,3 +1,4 @@
 export { CoreEditor } from '@kerebron/editor';
 export { AdvancedEditorKit } from '@kerebron/editor-kits/AdvancedEditorKit';
 export { createAssetLoad } from '@kerebron/wasm/web';
+export { ExtensionHistory } from '@kerebron/extension-basic-editor/ExtensionHistory';
